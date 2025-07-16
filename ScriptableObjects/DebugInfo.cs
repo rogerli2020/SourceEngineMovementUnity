50b3cec429f9daf9c2c2a518d344fe17e870269c
Roger Li <rel7817@nyu.edu>
2025-07-16 19:46:16 -0400

    deleted and renamed some files

--- ScriptableObjects/DebugInfo.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DebugInfo", menuName = "Scriptable Objects/DebugInfo")]
-public class DebugInfo : ScriptableObject
-{
-    public float speed;
-    public int fps;
-}
