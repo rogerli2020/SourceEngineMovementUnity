8365ee3678ce18cb0768ea7aac097388335349e2
Roger Li <rel7817@nyu.edu>
2025-07-17 13:52:29 -0400

    level and stats update

--- ScriptableObjects/MoveVars.cs
+++ ScriptableObjects/MoveVars.cs
@@ -7,13 +7,13 @@ public class MoveVars : ScriptableObject
     
     public float gravity = 35f;            
     public float stopSpeed = 2.5f;          
-    public float maxSpeed = 50f;      
+    public float maxSpeed = 10f;      
     public float groundAccel = 1f;
-    public float airAccel = 0.25f;
+    public float airAccel = 0.6f;
     public float slideAccel = 0.75f;
-    public float friction = 10;
+    public float friction = 7.5f;
     public float maxVelocity = 1000f;
-    public float moveSpeed = 8f;
+    public float moveSpeed = 7.5f;
     public float jumpVelocity = 10f;
     
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
