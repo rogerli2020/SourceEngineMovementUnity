c6280881200f4579d213def66d41f10d2d21bdba
Roger Li <rel7817@nyu.edu>
2025-11-07 19:59:37 -0500

    tweaked movevars but still doesnt feel right

--- ScriptableObjects/MoveVars.cs
+++ ScriptableObjects/MoveVars.cs
@@ -6,15 +6,15 @@ public class MoveVars : ScriptableObject
     public static MoveVars Instance;
     
     public float gravity = 35f;            
-    public float stopSpeed = 2.5f;          
+    public float stopSpeed = 10f;          
     public float maxSpeed = 10f;      
-    public float groundAccel = 1f;
-    public float airAccel = 0.6f;
-    public float slideAccel = 0.75f;
-    public float friction = 7.5f;
+    public float groundAccel = 5f;
+    public float airAccel = 0.7f;
+    public float slideAccel = 0.8f;
+    public float friction = 4f;
     public float maxVelocity = 1000f;
-    public float moveSpeed = 7.5f;
-    public float jumpVelocity = 10f;
+    public float moveSpeed = 8f;
+    public float jumpVelocity = 9f;
     
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     static void Init()
@@ -22,3 +22,15 @@ public class MoveVars : ScriptableObject
         Instance = Resources.Load<MoveVars>("MoveVars");
     }
 }
+
+
+
+// #define PM_GRAVITY 800 
+// #define PM_STOPSPEED 100 
+// #define PM_MAXSPEED 320 
+// #define PM_SPECTATORMAXSPEED 500 
+// #define PM_ACCELERATE 10 
+// #define PM_AIRACCELERATE 0.7 
+// #define PM_WATERACCELERATE 10 
+// #define PM_FRICTION 6 
+// #define PM_WATERFRICTION 1
